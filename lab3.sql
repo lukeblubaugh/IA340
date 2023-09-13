9224b39a9d567285907cafd396a64cf8a0dbd6bf
lukeblubaugh <122937245+lukeblubaugh@users.noreply.github.com>
2023-09-13 16:23:33 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -35,3 +35,30 @@ values('s1@jmu.edu','c1'),
 	('s3@jmu.edu','c3')
 ;
 select * from enroll
+
+--2.5
+insert into professor (p_email,p_name,office)
+values('p3@jmu.edu','p3','o3')
+;
+select * from professor
+;
+insert into course (c_number,c_name,room,p_email)
+values('c4','facebook','r1','p3@jmu.edu')
+;
+select * from course
+
+--You have to modify the professor table first, because the course table references the professor table.
+
+--2.6
+update course
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu'
+
+select * from course
+;
+delete from professor
+where p_name = 'p1'
+
+select * from professor
+
+--You must update the course table first because p1 cannot be deleted while teaching a course.
